f79c2be8b75983e015ced65f12c724763ce4a0f4
Brad J Herring <53147565+xiancn@users.noreply.github.com>
2024-01-30 20:12:18 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
 serve(() => {
-  return fetch(new URL("https://www.baidu.com/", import.meta.url));
+  return fetch(new URL("./Readme.md/", import.meta.url));
 });
